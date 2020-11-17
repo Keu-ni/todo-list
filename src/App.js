947ab9fe46c6cc10060ef28e94620df2f3a194b1
Keu-ni <skcor8909@gmail.com>
2020-11-18 00:19:02 +0900

    add context provider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { GlobalStyle } from './style';
 import { TodoTemplate, Header, TodoList, TodoCreate } from './components';
+import { TodoProvider } from './TodoContext';
 
 function App() {
   return (
-    <>
+    <TodoProvider>
       <GlobalStyle />
       <TodoTemplate>
         <Header />
         <TodoList />
         <TodoCreate />
       </TodoTemplate>
-    </>
+    </TodoProvider>
   );
 }
 
